8d6989c75e29812b188a294489c5b05397ed3e86
Raymond Harsono Alim <raymondalim2@gmail.com>
2025-12-14 14:17:23 +0700

    Add installation script for educational applications

---
+++ 02-install-aplikasi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Install aplikasi edukasi & produktivitas
+
+echo "[+] Menginstall aplikasi penting..."
+
+apt install -y \
+libreoffice \
+gimp \
+geogebra \
+vlc \
+evince \
+okular \
+filezilla \
+virt-manager \
+gnome-boxes \
+git \
+build-essential \
+htop
+
+echo "[✓] Aplikasi sudah terinstall dengan baik"
